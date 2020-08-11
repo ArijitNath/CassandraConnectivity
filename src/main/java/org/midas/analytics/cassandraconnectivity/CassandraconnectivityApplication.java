cb80e2b000f21f415a2ce8751c58b66dd1438045
Arijit Nath <arijitnath92@gmail.com>
2020-08-11 20:51:24 +0530

    Initial commit

---
+++ src/main/java/org/midas/analytics/cassandraconnectivity/CassandraconnectivityApplication.java
@@ -0,0 +1,13 @@
+package org.midas.analytics.cassandraconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraconnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraconnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/midas/analytics/cassandraconnectivity/CassandraconnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.midas.analytics.cassandraconnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraconnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
